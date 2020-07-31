3e7c3698005042ef3e034f90318ab07cfcea649c
Ivan Xing <xingyifan10@qq.com>
2020-07-31 15:55:44 +0800

    button加loading

--- src/button.vue
+++ src/button.vue
@@ -1,13 +1,37 @@
 <template>
-	<button class='s-button'>按钮</button>
+	<button class='s-button' :class="{[`icon-${iconPosition}`]: true}">
+		<s-icon class="icon" v-if="icon" :name="icon"></s-icon>
+		<s-icon class="loading" name="loading"></s-icon>
+		<div class="content">
+			<slot></slot>
+		</div>
+	</button>
 </template>
 
 <script>
-	export default {}
+	export default {
+		// props: ['icon', 'iconPosition']
+		props: {
+			icon: {},
+			iconPosition: {
+				type: String,
+				default: 'left',
+				validator(value) {
+					return value === 'left' || value === 'right';
+				}
+			}
+		}
+	}
 </script>
 
 <style lang="scss">
 	.s-button {
+
+		display: inline-flex;
+		justify-content: center;
+		align-items: center;
+		vertical-align: middle;
+
 		font-size: var(--font-size);
 		height: var(--button-height);
 		padding: 0 1em;
@@ -22,6 +46,31 @@
 		}
 		&:focus {
 			outline: none;
+		}
+		> .icon {
+			order: 1;
+			margin-right: .3em;
+		}
+		> .content {
+			order: 2;
+		}
+		&.icon-right {
+			> .icon {
+				order: 2;
+				margin-left: .3em;
+				margin-right: 0;
+			}
+			> .content {
+				order: 1;
+			}
+		}
+
+		@keyframes spin {
+			0% { transform: rotate(0deg); }
+			100% { transform: rotate(360deg); }
+		}
+		.loading{
+			animation: spin 2s infinite linear;  /* spin动画 2s 无限 线性 滚动*/
 		}
 	}
 </style>
