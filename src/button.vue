3f90acf3d614f62858c7db68baa764988977dd41
xingyifan <xingyifan10@qq.com>
2021-02-20 15:44:15 +0800

    完善icon和iconPositon属性

--- src/button.vue
+++ src/button.vue
@@ -1,29 +1,67 @@
 <template>
-  <button class='s-button'>
-    <slot></slot>
+  <button class='s-button' :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
   </button>
 </template>
 
 <script>
-  export default {}
+  export default {
+    props: ['icon', 'iconPosition']
+  }
 </script>
 
 <style lang="scss">
   .s-button {
     font-size: var(--font-size);
     height: var(--button-height);
-    padding: 0 1em;
+    padding: 0 .7em;
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    /* inline-flex 造成上下不对齐 所有inline元素都有此问题解决办法*/
+    vertical-align: middle;
+
     &:hover {
       border-color: var(--border-color-hover);
     }
+
     &:active {
       background-color: var(--button-active-bg);
     }
+
     &:focus {
       outline: none;
+    }
+
+    /*用css控制顺序 默认排序 多属性反转*/
+    > .icon {
+      order: 1;
+      margin-right: .3em;
+    }
+
+    > .content {
+      order: 2;
+    }
+
+    &.icon-right {
+      > .icon {
+        order: 2;
+        margin-left: .3em;
+        margin-right: 0;
+      }
+
+      > .content {
+        order: 1;
+      }
     }
   }
 </style>
